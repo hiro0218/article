25f02c9bbb531873fe0b7ebb4503f397fd4879a3
hiro0218 <hiro0218@gmail.com>
2023-05-30 21:04:34 +0900

    refactor

--- cli.js
+++ cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import fs from "fs-extra";
-import path from "path";
-import minimist from "minimist";
-import dayjs from "dayjs";
+import fs from 'fs-extra';
+import path from 'path';
+import minimist from 'minimist';
+import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc.js';
 import tz from 'dayjs/plugin/timezone.js';
 
@@ -10,41 +10,55 @@ dayjs.extend(utc);
 dayjs.extend(tz);
 dayjs.tz.setDefault('Asia/Tokyo');
 
+/**
+ * generate file
+ * @param {string} fullpath
+ * @param {string} content
+ */
 function generateFile(fullpath, content) {
   fs.outputFileSync(
     fullpath,
     content,
-    { flag: "wx" } // not overwrite
+    { flag: 'wx' }, // not overwrite
   );
+}
+
+/**
+ * create markdown file
+ * @param {string} filePath
+ * @param {dayjs.Dayjs} timestamp
+ */
+function createMarkdownFile(filePath, timestamp) {
+  const slug = timestamp.format('YYYYMMDDHHmm');
+  const fileName = `${slug}.md`;
+  const fullpath = path.join(filePath, `_posts/${fileName}`);
+  // prettier-ignore
+  const content =
+    [
+      "---",
+      `title: ""`,
+      `date: ${timestamp.utc().format()}`,
+      `updated: `,
+      `tags: `,
+      "---",
+    ].join("\n") + "\n";
+
+  generateFile(fullpath, content);
+
+  console.log(`generate: ${fullpath}`);
 }
 
 (async () => {
   try {
     const argv = minimist(process.argv.slice(2), {
       alias: {
-        n: "new",
+        n: 'new',
       },
     });
 
     // generate markdown
-    if (argv._.includes("n") || argv._.includes("new")) {
-      const now = dayjs();
-      const slug = now.format("YYYYMMDDHHmm");
-      const fileName = `${slug}.md`;
-      const filepath = path.join(process.cwd(), `_posts/${fileName}`);
-      const content =
-        [
-          "---",
-          `title: ""`,
-          `date: ${now.utc().format()}`,
-          `updated: `,
-          `tags: `,
-          "---",
-        ].join("\n") + "\n";
-
-      generateFile(filepath, content);
-
-      console.log(`generate: ${filepath}`);
+    if (argv._.includes('n') || argv._.includes('new')) {
+      createMarkdownFile(process.cwd(), dayjs());
     }
   } catch (error) {
     console.error(error);
